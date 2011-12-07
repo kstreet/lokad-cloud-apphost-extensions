88b898bf9529fcf68527a7f46122dbcd52d6d3a6
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-12-07 16:32:53 +0100

    Add empty FileDeployments extension, add AppHost.Framework dependency

---
+++ Source/FileDeployments/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿#region Copyright (c) Lokad 2009-2011
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lokad.Cloud.AppHost.Extensions.FileDeployments")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Lokad 2011")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyCompany("Lokad")]
+[assembly: AssemblyProduct("Lokad.Cloud.AppHost")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+[assembly: Guid("31d2a17f-986f-46ae-941e-25a78c384cb3")]
